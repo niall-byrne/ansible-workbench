8f9a7c315416747257b3ef2adfc425c63d85adf8
Niall Byrne <niall@niallbyrne.ca>
2021-05-12 11:36:14 -0400

    feat(COOKIECUTTER): separate project and galaxy slugs

--- .github/template.sh
+++ .github/template.sh
@@ -7,7 +7,7 @@ main() {
   pip install cookiecutter poetry
   git config --global user.name "${NAME}"
   git config --global user.email "${EMAIL}"
-  echo -e '\n\n\n\n\n\n\n' | cookiecutter template/
+  echo -e '\n\n\n\n\n\n\n\n\n' | cookiecutter template/
 }
 
 main
