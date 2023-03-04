7eb43eee9c5777a5172524dc72a5062c9464d062
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2023-03-04 10:19:37 -0500

    ci(GITHUB): switch to conventional-changelog-cli

--- {{cookiecutter.project_slug}}/.github/scripts/changelog.sh
+++ {{cookiecutter.project_slug}}/.github/scripts/changelog.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # .github/scripts/changelog.sh
-# Generates a changelog based off the diff between $TAG and the previous sequential tag.
+# Generates a changelog for the specified ${BRANCH_OR_TAG}.
 
-# TAG:  The new git tag the changelog is being generated for.
+# BRANCH_OR_TAG:  The new git tag the changelog is being generated for.
 
 # CI only script
 
@@ -11,18 +11,13 @@ set -eo pipefail
 
 main() {
 
-  echo "{}" > package.json
-
-  TAG="$(git tag -l --sort=refname | grep -E "[0-9]+\.[0-9]+\.[0-9]+" | tail -n 2 | head -n 1)"
-  CHANGE_LOG_CONTENT="$(npx -q generate-changelog -f - -t "${TAG}")"
+  CHANGE_LOG_CONTENT="$(npx -q conventional-changelog-cli -t "${BRANCH_OR_TAG}")"
 
   {
     echo "CHANGE_LOG_CONTENT<<EOF"
       echo "${CHANGE_LOG_CONTENT}"
     echo "EOF"
   } >> "${GITHUB_ENV}"
-
-  rm package.json
 
 }
 
